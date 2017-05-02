b119b1278007f8b3e401d8e0c544ff2590459e00
Chao Wang <kevin.mercury52@gmail.com>
2017-05-02 16:35:48 -0400

    Finish the Main Part
    Finish the main part, now the RMSE meets the requirement.

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -27,10 +27,10 @@ UKF::UKF() {
   P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+  std_a_ = 1.5;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
+  std_yawdd_ = 0.6;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
@@ -76,12 +76,12 @@ UKF::UKF() {
   // measurement noise convariance matrix
   R_radar_ = MatrixXd(3,3);
   R_radar_ << std_radr_ * std_radr_, 0, 0,
-  			  0, std_radphi_ * std_radphi_, 0,
-  			  0, 0, std_radrd_ * std_radrd_;
+  			      0, std_radphi_ * std_radphi_, 0,
+  			      0, 0, std_radrd_ * std_radrd_;
 
   R_lidar_ = MatrixXd(2,2);
   R_lidar_ << std_laspx_ * std_laspx_, 0,
-  			  0, std_laspy_ * std_laspy_;
+  			      0, std_laspy_ * std_laspy_;
 
 
 
@@ -103,10 +103,10 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   */
   if (!is_initialized_){
   	P_ << 1, 0, 0, 0, 0,
-  		  0, 1, 0, 0, 0,
-  		  0, 0, 1, 0, 0,
-  		  0, 0, 0, 1, 0,
-  		  0, 0, 0, 0, 1;
+  		    0, 1, 0, 0, 0,
+  		    0, 0, 1, 0, 0,
+  		    0, 0, 0, 1, 0,
+  		    0, 0, 0, 0, 1;
 
   	if (meas_package.sensor_type_ == MeasurementPackage::RADAR){
   		// convert radar from polar to cartesian and initialize state
@@ -129,8 +129,9 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   			x_(1) = EPS;
   		}
   	}
+
   	// Initialize weights
-    weights_(0) = lambda_ / (lambda_ + n_aug_);
+    weights_(0) = (1.0*lambda_) / (lambda_ + n_aug_);
     for (int i = 1; i < weights_.size(); i++) {
         weights_(i) = 0.5 / (n_aug_ + lambda_);
     }
@@ -141,6 +142,9 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
     // done initializing, no need to predict or update
     is_initialized_ = true;
 
+    //cout << "Init\n" << endl;
+    //cout << "x_\n" << x_ << endl;
+
     return;
 
   }
@@ -148,7 +152,11 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   double dt = (meas_package.timestamp_ - time_us_)/1000000.0;
 
   time_us_ = meas_package.timestamp_;
+
   Prediction(dt);
+
+  //cout << "predict:\n" << endl;
+  //cout << "x_\n" << endl << x_ << endl;
 
   if (meas_package.sensor_type_ == MeasurementPackage::RADAR && use_radar_){
   	UpdateRadar(meas_package);
@@ -191,34 +199,34 @@ void UKF::Prediction(double delta_t) {
 	// Square root of P_aug
 	MatrixXd L = P_aug.llt().matrixL();
   	//create augmented sigma points
-  	Xsig_aug.col(0) = x_aug;
-  	for (int i; i<n_aug_; i++){
-    	Xsig_aug.col(i+1)       = x_aug + sqrt(lambda_+n_aug_) * L.col(i);
-    	Xsig_aug.col(i+1+n_aug_) = x_aug - sqrt(lambda_+n_aug_) * L.col(i);
-  	}
+  Xsig_aug.col(0) = x_aug;
+  for (int i=0; i<n_aug_; i++){
+  	Xsig_aug.col(i+1)        = x_aug + sqrt(lambda_+n_aug_) * L.col(i);
+  	Xsig_aug.col(i+1+n_aug_) = x_aug - sqrt(lambda_+n_aug_) * L.col(i);
+  }
 
   	// predict sigma points
-  	for (int i; i<n_sig_; i++){
-  		// extract values for batter readability
-  		double p_x = Xsig_aug(0,i);
-      	double p_y = Xsig_aug(1,i);
-      	double v = Xsig_aug(2,i);
-      	double yaw = Xsig_aug(3,i);
-      	double yawd = Xsig_aug(4,i);
-      	double nu_a = Xsig_aug(5,i);
-      	double nu_yawdd = Xsig_aug(6,i);
+  for (int i=0; i<n_sig_; i++){
+  // extract values for batter readability
+    double p_x = Xsig_aug(0,i);
+    double p_y = Xsig_aug(1,i);
+    double v = Xsig_aug(2,i);
+    double yaw = Xsig_aug(3,i);
+    double yawd = Xsig_aug(4,i);
+    double nu_a = Xsig_aug(5,i);
+    double nu_yawdd = Xsig_aug(6,i);
       
-      	//predicted state values
-      	double px_p, py_p;
-     	//avoid division by zero
-      	if (fabs(yawd) > 0.001){
-          	px_p = p_x + v/yawd * (sin(yaw + yawd*delta_t) - sin(yaw));
-          	py_p = p_y + v/yawd * (cos(yaw) - cos(yaw + yawd*delta_t));
-      	}
-      	else{
-          	px_p = p_x + v*delta_t*cos(yaw);
-          	py_p = p_y + v*delta_t*sin(yaw);      
-      	}
+    //predicted state values
+    double px_p, py_p;
+    //avoid division by zero
+    if (fabs(yawd) > 0.001){
+      	px_p = p_x + v/yawd * (sin(yaw + yawd*delta_t) - sin(yaw));
+      	py_p = p_y + v/yawd * (cos(yaw) - cos(yaw + yawd*delta_t));
+    }
+    else{
+        px_p = p_x + v*delta_t*cos(yaw);
+        py_p = p_y + v*delta_t*sin(yaw);      
+    }
       
       	double v_p = v;
       	double yaw_p = yaw + yawd*delta_t;
